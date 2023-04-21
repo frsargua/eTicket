255de85e2d7151d1b44f4ecbaa064e8357eb5b9c
frsargua <frsargua@gmail.com>
2023-04-21 21:26:35 +0100

    Created accountController and added the dependencies properties.

---
+++ eTickets/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Data;
+using eTickets.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eTickets.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Used to work with the user related data.
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        //Used to check if the user is signed in.
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
